016c9f74344aa493b8da334226fa5621777730ac
Bryan Gula <bryandgula@gmail.com>
2015-01-13 03:49:47 -0800

    everything works except catchPropertyChange :q

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -3,7 +3,7 @@ package com.tedneward.example;
 import java.beans.*;
 import java.util.*;
 
-public class Person {
+public class Person implements Comparable<Person> {
   private int age;
   private String name;
   private double salary;
@@ -24,17 +24,38 @@ public class Person {
     return age;
   }
   
+  public void setAge(int age) {
+    	if (age < 0) {
+		throw new IllegalArgumentException();
+	} else {
+		this.age = age;
+	}
+  }
+
   public String getName() {
     return name;
   }
   
+  public void setName(String name) {
+	if (name == null) {
+		throw new IllegalArgumentException();
+	} else {
+		this.name = name;
+	}
+  }
+
   public double getSalary() {
     return salary;
+  }
+
+  public void setSalary(double salary) {
+	this.salary = salary;
   }
   
   public String getSSN() {
     return ssn;
   }
+
   public void setSSN(String value) {
     String old = ssn;
     ssn = value;
@@ -51,19 +72,47 @@ public class Person {
   }
   
   public String becomeJudge() {
-    return "The Honorable " + name;
+    return "The Honorable " + this.name;
   }
   
   public int timeWarp() {
     return age + 10;
   }
-  
-  public boolean equals(Person other) {
-    return (this.name.equals(p.name) && this.age == p.age);
+
+  public static ArrayList<Person> getNewardFamily() {
+	ArrayList<Person> list = new ArrayList<Person>();
+	list.add(new Person("Ted",41,250000.));
+	list.add(new Person("Charlotte",43,150000.));
+	list.add(new Person("Michael",22,10000.));
+	list.add(new Person("Matthew",15,0.));
+
+	return list;
   }
 
-  public String tostring() {
-    return "{{FIXME}}";
+  
+  @Override	
+  public boolean equals(Object obj) {
+	if (obj instanceof Person) {
+		Person per = (Person)obj;
+		return (this.name.equals(per.name) && this.age == per.age); 
+	}
+    return false;
+  }
+
+  @Override
+  public int compareTo(Person other) {
+  	if(getSalary() < other.getSalary()) {
+		return 1;
+	} else if (getSalary() > other.getSalary()) {
+		return -1;
+	} else {
+		return 0;
+	}
+  }
+
+  @Override 
+  public String toString() {
+    return "[Person name:" + getName() + " age:" + getAge() + " salary:" + getSalary() + "]";
   }
 
   // PropertyChangeListener support; you shouldn't need to change any of
@@ -76,4 +125,14 @@ public class Person {
   public void removePropertyChangeListener(PropertyChangeListener listener) {
       this.pcs.removePropertyChangeListener(listener);
   }
+ 
+  	public static class AgeComparator implements Comparator<Person> {
+		
+		public int compare(Person p1, Person p2) {
+			return p1.getAge() - p2.getAge();
+		}
+  	}	
+
+
+
 }
